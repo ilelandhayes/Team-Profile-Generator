bd552d97527bde17805635be1f2b6443a92d6edf
Leland Hayes <lelandkhayes13@gmail.com>
2021-08-12 15:32:26 -0400

    added functions to index.js file to generate an index.html file, also added questions.js to hold the array of question

---
+++ index.js
@@ -0,0 +1,26 @@
+const inquirer = require('inquirer');
+const questions = require('./lib/questions.js');
+const generateMarkdown = require('./utils/generateMarkdown');
+const fs = require('fs');
+
+// function that writes HTML file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (error) => {
+        (error) ? log.error(error) : console.log('Successfully wrote to index.html file')
+    })
+}
+
+function init() {
+
+    inquirer.prompt(questions)
+    .then(data => {
+        markdown = generateHTML(data);
+    })
+    .then(data => {
+        writeToFile("./dist/test-index.html", markdown);
+    })
+    .catch(err => console.log(err));
+
+}
+
+init();
