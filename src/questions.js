b1fb37acc6dd3e8be57987cb74160d546f51a9d4
Leland Hayes <lelandkhayes13@gmail.com>
2021-08-13 15:03:03 -0400

    updated question arrays, also updated the html generator

--- src/questions.js
+++ src/questions.js
@@ -1,9 +1,64 @@
-const questions = [
+const employeeQuestions = [
     {
-        name: "employeename",
+        name: "name",
         type: "input",
-        message: "What is your name?"
-    }
-]
+        message: "What is your team members name?",
+    },
+    {
+        name: "role",
+        type: "list",
+        message: "What is your team members role?",
+        choices: ["Manager", "Intern", "Engineer"],
+    },
+    {
+        name: "id",
+        type: "input",
+        message: "What is your team member ID?",
+    },
+    {
+        name: "email",
+        type: "input",
+        message: "What is your team members email?",
+    },
+];
 
-module.exports = questions;
+const engineerQuestions = [
+    {
+        name: "github",
+        type: "input",
+        message: "What is the engineers Github?"
+    },
+];
+
+const managerQuestions = [
+    {
+        name: "officeNumer",
+        type: "input",
+        message: "What is the managers office number?"
+    },
+];
+
+const internQuestions = [
+    {
+        name: "school",
+        type: "input",
+        message: "What school does the intern attend?"
+    },
+];
+
+const addNewTeamMemberQuestions = [
+    {
+        name: "addNewMember",
+        type: "list",
+        message: "Would you like to add a new team member?",
+        choices: ["Yes", "No"]
+    },
+];
+
+module.exports = {
+    employeeQuestions, 
+    engineerQuestions, 
+    managerQuestions,
+    internQuestions,
+    addNewTeamMemberQuestions
+};
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,72 @@
+const managerCard = (managerArray) => {
+    let card = `
+    <div class="card text-white bg-dark mb-3" style="max-width: 18rem;">
+        <div class="card-header">
+            <div class="card-body">
+                <h5 class="card-title">${manager.name}</h5>
+                    <p class="subtitle has-text-weight-bold is-6 has-text-dark">
+                        Manager
+                    </p>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">ID: ${manager.id}</li>
+                    <li class="list-group-item">Email: ${manager.email}</li>
+                    <li class="list-group-item">Office Number: ${manager.officeNumber}</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    `;
+    return card;
+}
 
-function generateHTML(data) {
+const internCard = (internArray) => {
+    let card = `
+    <div class="card text-white bg-dark mb-3" style="max-width: 18rem;">
+        <div class="card-header">
+            <div class="card-body">
+                <h5 class="card-title">${intern.name}</h5>
+                    <p class="subtitle has-text-weight-bold is-6 has-text-dark">
+                        Manager
+                    </p>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">ID: ${intern.id}</li>
+                    <li class="list-group-item">Email: ${intern.email}</li>
+                    <li class="list-group-item">Office Number: ${intern.officeNumber}</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    `;
+    return card;
+}
 
-    let markdown = `
+const engineerCard = (engineerArray) => {
+    let card = `
+    <div class="card text-white bg-dark mb-3" style="max-width: 18rem;">
+        <div class="card-header">
+            <div class="card-body">
+                <h5 class="card-title">${engineer.name}</h5>
+                    <p class="subtitle has-text-weight-bold is-6 has-text-dark">
+                        Manager
+                    </p>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">ID: ${engineer.id}</li>
+                    <li class="list-group-item">Email: ${engineer.email}</li>
+                    <li class="list-group-item">Office Number: ${engineer.officeNumber}</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    `;
+    return card;
+}
+
+
+
+
+const generateHTML = (managerArray, internArray, engineerArray) => {
+
+    return `
         <!DOCTYPE html>
     <html lang="en">
     <head>
@@ -20,24 +85,23 @@ function generateHTML(data) {
     </header>
 
     <body>
-        <div class="card text-white bg-dark mb-3" style="max-width: 18rem;">
-        <div class="card-header"></div>
-        <div class="card-body">
-            <h5 class="card-title">Dark card title</h5>
-            <ul class="list-group list-group-flush">
-                <li class="list-group-item">An item</li>
-                <li class="list-group-item">A second item</li>
-                <li class="list-group-item">A third item</li>
-            </ul>
+        <div class="hero-body">
+            <div class="columns">
+                <div class="column">
+                    ${managerCard(managerArray)}
+                <div class="column">
+                    ${engineerCard(engineerArray)}
+                <div class="column">
+                    ${internCard(internArray)}
+            </div>
         </div>
-    </div>
 
 
     
     </body>
+    </html>
     `;
 
-    return markdown;
 }
 
 module.exports = generateHTML;
